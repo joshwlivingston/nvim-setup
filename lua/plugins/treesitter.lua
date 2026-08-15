90372101044a24d9a5634c556e5a99c9ec69455e
Josh Livingston <joshwlivingston@gmail.com>
2026-08-15 10:31:25 -0600

    remove r from treesitter requirements

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,26 +3,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = {
-        "csv",
-        "kotlin",
-        "latex",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "r",
-        "rnoweb",
-        "xml",
-        "yaml",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
+        ensure_installed = {
+            "csv",
+            "hcl",
+            "kotlin",
+            "latex",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "terraform",
+            "xml",
+            "yaml",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
